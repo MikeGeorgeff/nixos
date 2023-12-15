70474410febce698834ef702cf6ab030e87c1931
Mike Georgeff <mike@georgeff.co>
2023-12-15 09:54:55 -0600

    configure container podman networking

--- hosts/modules/podman.nix
+++ hosts/modules/podman.nix
@@ -5,6 +5,14 @@
     dockerCompat = true;
     dockerSocket.enable = true;
     autoPrune.enable = true;
+    defaultNetwork.settings = {
+      dns_enabled = true;
+    };
+  };
+
+  networking.firewall.interfaces."podman0" = {
+    allowedUDPPorts = [ 53 ];
+    allowedTCPPorts = [ 53 ];
   };
 
   virtualisation.oci-containers.backend = "podman";
